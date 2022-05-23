078d892e147c36b1814ed9ba8a8af687969a0c29
griegner <johndoe@example.com>
2022-05-23 10:19:34 -0700

    make apply_ps reusable module

--- workflow/scripts/00_cbf-to-ps.py
+++ workflow/scripts/00_cbf-to-ps.py
@@ -1,36 +1,16 @@
 import argparse
 from pathlib import Path
 
-import numpy as np
 import pandas as pd
 from nilearn import image
-from nilearn.maskers import NiftiMasker
+
+import apply_ps
 
 
 def replace_path(path, append):
     "replace the extension of a bids path"
     return path.parent / (path.stem.rsplit("_", 4)[0] + append)
 
-def _dot_product(cbf_img, ps_img):
-    "compute the dot product of cbf and pain sig images"
-    masker = NiftiMasker(image.binarize_img(ps_img), reports=False)
-    ps_vect = masker.fit_transform(ps_img)[0]
-    cbf_vect = masker.fit_transform(cbf_img)
-    return float(np.dot(cbf_vect, ps_vect))
-
-def apply_ps(cbf_img, pain_sigs=["nps", "siips"]):
-    "apply each pain sig to the cbf image"
-    ps_response = {}
-    for ps in pain_sigs:
-        ps_img = image.load_img(f"data/pain_sigs/{ps}.nii.gz")
-        if ps == "nps":
-            cbf_img_resamp = image.resample_to_img(
-                cbf_img, ps_img, interpolation="continuous"
-            )
-            ps_response[ps] = _dot_product(cbf_img_resamp, ps_img)
-        else:
-            ps_response[ps] = _dot_product(cbf_img, ps_img)
-    return ps_response
 
 def main(args):
 
@@ -46,7 +26,7 @@ def main(args):
     if args.smooth_fwhm:
         diff = image.smooth_img(diff, args.smooth_fwhm)
 
-    ps_response = apply_ps(diff, ['nps', 'siips'])
+    ps_response = apply_ps.apply_ps(diff, ['nps', 'siips'])
     df.join(pd.DataFrame([ps_response])).to_csv(args.output, index=False)
 
 
---
+++ workflow/scripts/apply_ps.py
@@ -0,0 +1,25 @@
+import numpy as np
+from nilearn import image
+from nilearn.maskers import NiftiMasker
+
+
+def _dot_product(img, ps_img):
+    "compute the dot product of cbf and pain signature images"
+    masker = NiftiMasker(image.binarize_img(ps_img), reports=False)
+    ps_vect = masker.fit_transform(ps_img)[0]
+    img_vect = masker.fit_transform(img)
+    return float(np.dot(img_vect, ps_vect))
+
+def apply_ps(img, pain_sigs=["nps", "siips"]):
+    "apply each pain signature to the input image"
+    ps_response = {}
+    for ps in pain_sigs:
+        ps_img = image.load_img(f"data/pain_sigs/{ps}.nii.gz")
+        if ps == "nps":
+            img_resamp = image.resample_to_img(
+                img, ps_img, interpolation="continuous"
+            )
+            ps_response[ps] = _dot_product(img_resamp, ps_img)
+        else:
+            ps_response[ps] = _dot_product(img, ps_img)
+    return ps_response
